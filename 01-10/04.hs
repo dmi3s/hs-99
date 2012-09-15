1f43882eb370d7e77cabab6f5679999c584c9a39
Sergey (dmi3s) Dmitriev <dmi3evsv@gmail.com>
2012-09-16 03:38:19 +0400

    04

---
+++ 01-10/04.hs
@@ -0,0 +1,11 @@
+myLength :: [a] -> Int
+
+myLength [] =  0
+myLength (_:xs) = 1 + myLength xs
+
+
+myLength' xs = myLengthImpl xs 0 where
+  myLengthImpl [] n = n
+  myLengthImpl (_:xs) n = myLengthImpl xs n+1
+
+myLength'' = sum . map (\_ -> 1)
