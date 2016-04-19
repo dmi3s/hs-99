23f6392c161a91821d822cfa4da09fe88b8ad3d3
dmi3s <dmi3evsv@gmail.com>
2016-04-19 17:05:43 +0300

    After lint

--- 01-10/01.hs
+++ 01-10/01.hs
@@ -3,6 +3,6 @@
 myLast :: [a] -> a
 
 myLast []     = error "empty list"
-myLast (x:[]) = x
+myLast [x] = x
 myLast (_:xs) = myLast xs
 
--- 01-10/04.hs
+++ 01-10/04.hs
@@ -8,4 +8,4 @@ myLength' xs = myLengthImpl xs 0 where
   myLengthImpl [] n = n
   myLengthImpl (_:xs) n = myLengthImpl xs n+1
 
-myLength'' = sum . map (\_ -> 1)
+myLength'' = sum . map (const 1)
